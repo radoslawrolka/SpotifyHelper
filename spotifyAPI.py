ffd687c105f8ad8ba54d4197e9215eb394ae5972
Radosław Rolka <radoslaw.rolka@gmail.com>
2023-09-11 11:24:51 +0200

    Spotify API token and credentials functionalities

---
+++ spotifyAPI.py
@@ -0,0 +1,47 @@
+import base64
+import json
+from os import path
+from requests import get, post
+
+# Spotify API credentials ----------------------------------------------------------------------------------------------
+def get_credentials(clientID, secretID):
+    with open('.credentials', 'w') as f:
+        f.write(clientID + '\n')
+        f.write(secretID + '\n')
+    return True
+
+def load_credentials():
+    with open('.credentials') as f:
+        CLIENT_ID = f.readline().strip()
+        CLIENT_SECRET = f.readline().strip()
+    return CLIENT_ID, CLIENT_SECRET
+
+# Spotify API token ----------------------------------------------------------------------------------------------------
+def create_token():
+    CLIENT_ID, CLIENT_SECRET = load_credentials()
+    auth_str = CLIENT_ID + ':' + CLIENT_SECRET
+    auth_bytes = auth_str.encode("utf-8")
+    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
+
+    url = "https://accounts.spotify.com/api/token"
+    headers = {
+        "Authorization": "Basic " + auth_base64,
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    data = {
+        "grant_type": "client_credentials"
+    }
+    result = post(url, headers=headers, data=data)
+    json_result = json.loads(result.content)
+    token = json_result["access_token"]
+    with open(".token", "w") as f:
+        f.write(token)
+    return token
+
+def get_token():
+    if path.getmtime(".token") < 3600:
+        with open(".token", "r") as f:
+            token = f.read()
+        return token
+    else:
+        return create_token()
