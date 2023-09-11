f312089d100b771e71d94630af220972d3487db5
Radosław Rolka <radoslaw.rolka@gmail.com>
2023-09-11 17:46:23 +0200

    scraping Spotify most streamed artists

--- spotifyCharts.py
+++ spotifyCharts.py
@@ -25,11 +25,26 @@ def get_top_songs():
 
 # Get most streamed artists --------------------------------------------------------------------------------------------
 def get_top_artists():
-    pass
+    url = 'https://chartmasters.org/most-streamed-artists-ever-on-spotify/?slk=hp'
+    r = requests.get(url)
+    data = r.text
+    data = data[data.find('table_1_row_0'):data.find('!-- /Table body -->')]
+
+    soup = BeautifulSoup(data, 'html.parser')
+    td_elements = soup.find_all('td', {'style': ''})
+
+    top_artists = []
+    for i in range(0, len(td_elements), 16):
+        pos = td_elements[i+1].get_text(strip=True)
+        artist = td_elements[i + 3].get_text(strip=True)
+        streams = td_elements[i + 11].get_text(strip=True)
+        top_artists.append((pos, artist, streams))
+
+    return top_artists
 
 
 # Get most streamed albums ---------------------------------------------------------------------------------------------
 
-get_top_songs()
-# get_top_artists()
+#get_top_songs()
+get_top_artists()
 # get_top_albums()
